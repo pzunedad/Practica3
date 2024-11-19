5de4e59576b821655acbde38695eced236f19d45
pzunedad <pedro.zunedadiego@gmail.com>
2024-11-19 14:11:36 +0100

    Changes in main and utils solution errors

--- main.ts
+++ main.ts
@@ -16,26 +16,6 @@ const db = client.db("Ubicacion");
 
 //const personasCollection = db.collection<personasModel>("personas");
 //const friendsCollection = db.collection<friendsModel>("friends");
-/*
-const haversine = (lat1, lon1, lat2, lon2) => {
-    const R = 6371; // Radio de la Tierra en km
-    const toRad = (deg) => (deg * Math.PI) / 180; // Conversión a radianes
-
-    const dLat = toRad(lat2 - lat1);
-    const dLon = toRad(lon2 - lon1);
-    const lat1Rad = toRad(lat1);
-    const lat2Rad = toRad(lat2);
-    
-    const a = Math.sin(dLat / 2) ** 2 +
-        Math.cos(lat1Rad) * Math.cos(lat2Rad) *
-        Math.sin(dLon / 2) ** 2;
-    
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    
-    return R * c; // Distancia en km
-
-}
-*/
 
 const handler = async (req:Request): Promise<Response> =>{
     const method = req.method;
--- utils.ts
+++ utils.ts
@@ -5,21 +5,17 @@ export const fromModelToNino = async (
   NinoDB: NinoModel,
   LugarCollection: Collection<LugarModel>
 ): Promise<Nino> => {
-    const ubicacion = await LugarCollection
-    .findOne({ _id: NinoDB.ubicacion }); // Buscar un solo ID de ubicación
-
-  // Si no se encuentra la ubicación, retornamos null o manejamos el error
+  const ubicacion = await LugarCollection.findOne({ _id: NinoDB.ubicacion });
   if (!ubicacion) {
-    throw new Error(`Ubicación con ID ${NinoDB.ubicacion} no encontrada`);
+      throw new Error("Location not found");
   }
-
   return {
-    id: NinoDB._id!.toString(),
-    nombre: NinoDB.nombre,
-    comportamiento: NinoDB.comportamiento,
-    ubicacion: ubicacion.map((b) => fromModelToLugar(b)),
+      id: NinoDB._id!.toString(),
+      nombre: NinoDB.nombre,
+      comportamiento: NinoDB.comportamiento,
+      ubicacion: fromModelToLugar(ubicacion),
   };
-};
+}
 
 export const fromModelToLugar = (model: LugarModel): Lugar => ({
   id: model._id!.toString(),
@@ -27,3 +23,18 @@ export const fromModelToLugar = (model: LugarModel): Lugar => ({
   coordenadas: model.coordenadas,
   numero_ninos_buenos: model.numero_ninos_buenos,
 });
+
+export const haversine = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
+  const R = 6371; // Radio de la Tierra en km
+  const toRad = (deg: number) => (deg * Math.PI) / 180; // Conversión a radianes
+
+  const dLat = toRad(lat2 - lat1);
+  const dLon = toRad(lon2 - lon1);
+  const lat1Rad = toRad(lat1);
+  const lat2Rad = toRad(lat2);
+
+  const a = Math.sin(dLat / 2) ** 2 + Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.sin(dLon / 2) ** 2;
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c; // Distancia en km
+};
