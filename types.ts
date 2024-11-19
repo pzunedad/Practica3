7fe82b0c968d65ae5e52c46ba1410677d90cccd8
pzunedad <pedro.zunedadiego@gmail.com>
2024-11-19 13:44:33 +0100

    update comportamiento y coordenadas

--- types.ts
+++ types.ts
@@ -1,34 +1,34 @@
 import {ObjectId, type OptionalId} from "mongodb";
 
-export enum Estado{
-    bueno = "bueno",
-    malo = "malo"
-}
-
-
 export type Nino = {
     id: string;
     nombre: string; // Nombre del niño (unico y obligatorio)
-    comportamiento: Estado; // Comportamiento del niño
+    comportamiento: "bueno" | "malo"; // Comportamiento del niño
     ubicacion: Lugar; // ID de la ubicación donde se encuentra el niño
   };
   
 export type Lugar = {
     id: string;
     nombre: string; // Nombre del lugar (único y obligatorio)
-    coordenadas: Estado; // Coordenadas del lugar
+    coordenadas: {
+      lat:number,
+      lon:number,
+  }, // Coordenadas del lugar
     numero_ninos_buenos: number; // Número de niños buenos asociados a este lugar
 };
 
 export type NinoModel = OptionalId<{
     nombre: string; 
-    comportamiento: Estado; 
+    comportamiento: "bueno" | "malo"; 
     ubicacion: ObjectId; 
   }>;
   
 export type LugarModel = OptionalId<{
     nombre: string; 
-    coordenadas: Estado; 
+    coordenadas: {
+        lat:number,
+        lon:number,
+    },
     numero_ninos_buenos: number; 
 }>;
   
